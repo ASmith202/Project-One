697c815f896e3cb682e36ce1f2a2e179b5a5ef4b
CCL <anicias@yahoo.com>
2025-01-14 20:11:49 -0500

    revisions

---
+++ script.js
@@ -0,0 +1,25 @@
+
+    function selectProtein(protein) {
+        alert("You selected: " + protein)
+    }
+
+
+    function selectCondiment(condiment) {
+        alert("You selected: " + condiment)
+    }
+
+function selectTopping(topping) {
+    alert("You selected topping: " + topping);
+}
+
+function selectCheese(cheese) {
+    alert("You selected cheese: " + cheese);
+}
+
+function selectBread(bread) {
+    alert("You selected bread: " + bread);
+}
+
+function selectSize(size) {
+    alert("You selected size: " + size);
+}
