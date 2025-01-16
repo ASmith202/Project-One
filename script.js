e77c929e4166bbafd423ba32cbd67a044115ccc8
CCL <anicias@yahoo.com>
2025-01-15 20:42:42 -0500

    added dom manipulation

--- script.js
+++ script.js
@@ -23,3 +23,294 @@ function selectBread(bread) {
 function selectSize(size) {
     alert("You selected size: " + size);
 }
+
+function selectSize(button) {
+    let buttons = button.parentElement.getElementsByClassName('ingredient-button');
+    
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove('selected');
+    }
+    
+    button.classList.add('selected');
+}
+
+function selectProtein(button) {
+    let buttons = button.parentElement.getElementsByClassName('ingredient-button');
+    
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove('selected');
+    }
+    
+    button.classList.add('selected');
+}
+
+function selectCheese(button) {
+    let buttons = button.parentElement.getElementsByClassName('ingredient-button');
+    
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove('selected');
+    }
+    
+    button.classList.add('selected');
+}
+
+function selectTopping(button) {
+    let buttons = button.parentElement.getElementsByClassName('ingredient-button');
+    
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove('selected');
+    }
+    
+    button.classList.add('selected');
+}
+
+function selectCondiment(button) {
+    let buttons = button.parentElement.getElementsByClassName('ingredient-button');
+    
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove('selected');
+    }
+    
+    button.classList.add('selected');
+}
+
+function selectBread(button) {
+    let buttons = button.parentElement.getElementsByClassName('ingredient-button');
+    
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove('selected');
+    }
+    
+    button.classList.add('selected');
+}
+
+
+
+function selectProtein(button) {
+    let buttons = button.parentElement.getElementsByClassName('Protein-button');
+    
+    
+    
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove('selected');
+    }
+    
+    button.classList.add('selected');
+    
+    const selectedItem = button.textContent;
+
+    if (!isItemAlreadySelected(selectedItem)) {
+
+        const li = document.createElement('li');
+        li.textContent = selectedItem;
+
+        document.getElementById('custom-sub-list').appendChild(li);
+    }
+}
+
+function isItemAlreadySelected(item) {
+    const listItems = document.getElementById('custom-sub-list').getElementsByTagName('li');
+    for (let i = 0; i < listItems.length; i++) {
+        if (listItems[i].textContent === item) {
+            return true; 
+        }
+    }
+    return false;
+}
+
+
+function selectCheese(button) {
+    let buttons = button.parentElement.getElementsByClassName('Cheese-button');
+    
+    
+    
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove('selected');
+    }
+    
+    button.classList.add('selected');
+    
+    const selectedItem = button.textContent;
+
+    if (!isItemAlreadySelected(selectedItem)) {
+
+        const li = document.createElement('li');
+        li.textContent = selectedItem;
+
+        document.getElementById('custom-sub-list').appendChild(li);
+    }
+}
+
+function isItemAlreadySelected(item) {
+    const listItems = document.getElementById('custom-sub-list').getElementsByTagName('li');
+    for (let i = 0; i < listItems.length; i++) {
+        if (listItems[i].textContent === item) {
+            return true; 
+        }
+    }
+    return false;
+}
+
+
+
+function selectCondiment(button) {
+    let buttons = button.parentElement.getElementsByClassName('Condiment-button');
+    
+
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove('selected');
+    }
+    
+    button.classList.add('selected');
+    
+    const selectedItem = button.textContent;
+
+    if (!isItemAlreadySelected(selectedItem)) {
+
+        const li = document.createElement('li');
+        li.textContent = selectedItem;
+
+        document.getElementById('custom-sub-list').appendChild(li);
+    }
+}
+
+function isItemAlreadySelected(item) {
+    const listItems = document.getElementById('custom-sub-list').getElementsByTagName('li');
+    for (let i = 0; i < listItems.length; i++) {
+        if (listItems[i].textContent === item) {
+            return true; 
+        }
+    }
+    return false;
+}
+
+
+function selectTopping(button) {
+    button.classList.toggle('selected');
+
+    const selectedItem = button.textContent;
+
+    if (button.classList.contains('selected') && !isItemAlreadySelected(selectedItem)) {
+        const li = document.createElement('li');
+        li.textContent = selectedItem;
+        document.getElementById('custom-sub-list').appendChild(li);
+    } 
+    else if (!button.classList.contains('selected')) {
+        const listItems = document.getElementById('custom-sub-list').getElementsByTagName('li');
+        for (let i = 0; i < listItems.length; i++) {
+            if (listItems[i].textContent === selectedItem) {
+                listItems[i].remove();
+                break;
+            }
+        }
+    }
+}
+
+
+
+function selectTopping(button) {
+    if (!breadSelected) {
+        alert("Please select a bread option first!");
+        return; 
+    }
+
+    button.classList.toggle('selected');
+
+    const selectedItem = button.textContent;
+
+    if (button.classList.contains('selected') && !isItemAlreadySelected(selectedItem)) {
+        const li = document.createElement('li');
+        li.textContent = selectedItem;
+        document.getElementById('custom-sub-list').appendChild(li);
+    } 
+    else if (!button.classList.contains('selected')) {
+        const listItems = document.getElementById('custom-sub-list').getElementsByTagName('li');
+        for (let i = 0; i < listItems.length; i++) {
+            if (listItems[i].textContent === selectedItem) {
+                listItems[i].remove();
+                break;
+            }
+        }
+    }
+}
+
+function isItemAlreadySelected(item) {
+    const listItems = document.getElementById('custom-sub-list').getElementsByTagName('li');
+    for (let i = 0; i < listItems.length; i++) {
+        if (listItems[i].textContent === item) {
+            return true; 
+        }
+    }
+    return false;
+}
+
+
+
+
+let breadSelected = false; 
+let sizeSelected = false;
+
+function selectBread(button) {
+    let buttons = button.parentElement.getElementsByClassName('Bread-button');
+
+   
+    if (!breadSelected) {
+        let sizeButtons = document.querySelectorAll('.Size-button.selected');
+        sizeButtons.forEach(function (sizeButton) {
+            sizeButton.classList.remove('selected');
+        });
+        breadSelected = true;
+    }
+
+
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove('selected');
+    }
+
+ 
+    button.classList.add('selected');
+    const selectedItem = button.textContent;
+
+    if (!isItemAlreadySelected(selectedItem)) {
+        const li = document.createElement('li');
+        li.textContent = selectedItem;
+        document.getElementById('custom-sub-list').appendChild(li);
+    }
+}
+
+function selectSize(button) {
+    let buttons = button.parentElement.getElementsByClassName('Size-button');
+
+    if (!sizeSelected) {
+        let breadButtons = document.querySelectorAll('.Bread-button.selected');
+        breadButtons.forEach(function (breadButton) {
+            breadButton.classList.remove('selected');
+        });
+        sizeSelected = true;
+    }
+
+
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove('selected');
+    }
+
+   
+    button.classList.add('selected');
+    const selectedItem = button.textContent;
+
+    if (!isItemAlreadySelected(selectedItem)) {
+        const li = document.createElement('li');
+        li.textContent = selectedItem;
+        document.getElementById('custom-sub-list').appendChild(li);
+    }
+}
+
+function isItemAlreadySelected(item) {
+    const listItems = document.getElementById('custom-sub-list').getElementsByTagName('li');
+    for (let i = 0; i < listItems.length; i++) {
+        if (listItems[i].textContent === item) {
+            return true; 
+        }
+    }
+    return false;
+}
